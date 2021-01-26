80797ca4448ded7c28f89b161aba36231f61961e
1996mihir <mihir.joshi@ves.ac.in>
2021-01-25 23:22:07 -0600

    Test

---
+++ GitTestEclipse/src/Mypackage/Test.java
@@ -0,0 +1,14 @@
+package Mypackage;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=45;
+		int c=a+b;
+		System.out.println("Answer is "+c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
